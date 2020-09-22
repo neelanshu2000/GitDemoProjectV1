506f7b4abe9bf3ac0c2816cf1db0cbff6edc10bf
Neelanshu Dhar <Neelanshu Dhar@LAPTOP-418FVF2J>
2020-09-22 12:07:09 +0530

    My First Commit

---
+++ src/packgit/code1.java
@@ -0,0 +1,18 @@
+package packgit;
+
+public class code1 
+{
+
+	public static void main(String[] args) {
+		String sg="nel";
+			if(sg =="nel")
+			{
+				System.out.println("Success");
+			}
+			else
+			{
+				System.out.println("try again");
+			}
+	}
+
+}
